e9a98c99eaa1cbd64b91cbf698e4b251b413ee25
Presti <dxssucuk@hotmail.com>
2023-02-10 11:38:25 +0100

    Change the exception to contain the internal object.

--- src/main/java/de/presti/wrapper/entities/VideoResult.java
+++ src/main/java/de/presti/wrapper/entities/VideoResult.java
@@ -29,54 +29,59 @@ public class VideoResult {
 
     public VideoResult(JsonObject jsonObject, boolean importFromChannel, boolean importFromShort) {
         internalObject = jsonObject;
-        if (importFromChannel) {
-            id = jsonObject.getAsJsonPrimitive("videoId").getAsString();
-            JsonArray thumbnailArray = jsonObject.getAsJsonObject("thumbnail").getAsJsonArray("thumbnails");
-            thumbnail = thumbnailArray.get(thumbnailArray.size() - 1).getAsJsonObject()
-                    .getAsJsonPrimitive("url").getAsString();
-            viewCountText = jsonObject.getAsJsonObject("viewCountText").getAsJsonPrimitive("simpleText").getAsString();
-
-            if (importFromShort) {
-                title = jsonObject.getAsJsonObject("headline").getAsJsonPrimitive("simpleText").getAsString();
-            } else {
-                title = jsonObject.getAsJsonObject("title").getAsJsonArray("runs").get(0).getAsJsonObject()
-                        .getAsJsonPrimitive("text").getAsString();
-                durationText = jsonObject.getAsJsonObject("lengthText").getAsJsonPrimitive("simpleText").getAsString();
+        try {
+            if (importFromChannel) {
+                id = jsonObject.getAsJsonPrimitive("videoId").getAsString();
+                JsonArray thumbnailArray = jsonObject.getAsJsonObject("thumbnail").getAsJsonArray("thumbnails");
+                thumbnail = thumbnailArray.get(thumbnailArray.size() - 1).getAsJsonObject()
+                        .getAsJsonPrimitive("url").getAsString();
                 viewCountText = jsonObject.getAsJsonObject("viewCountText").getAsJsonPrimitive("simpleText").getAsString();
 
-                if (jsonObject.has("descriptionSnippet")) {
-                    descriptionSnippet = jsonObject.getAsJsonObject("descriptionSnippet").getAsJsonArray("runs").get(0).getAsJsonObject()
+                if (importFromShort) {
+                    title = jsonObject.getAsJsonObject("headline").getAsJsonPrimitive("simpleText").getAsString();
+                } else {
+                    title = jsonObject.getAsJsonObject("title").getAsJsonArray("runs").get(0).getAsJsonObject()
                             .getAsJsonPrimitive("text").getAsString();
+                    durationText = jsonObject.getAsJsonObject("lengthText").getAsJsonPrimitive("simpleText").getAsString();
+                    viewCountText = jsonObject.getAsJsonObject("viewCountText").getAsJsonPrimitive("simpleText").getAsString();
+
+                    if (jsonObject.has("descriptionSnippet")) {
+                        descriptionSnippet = jsonObject.getAsJsonObject("descriptionSnippet").getAsJsonArray("runs").get(0).getAsJsonObject()
+                                .getAsJsonPrimitive("text").getAsString();
+                    }
                 }
+            } else {
+                JsonArray formats = jsonObject.getAsJsonObject("streamingData").getAsJsonArray("formats");
+
+                if (formats != null)
+                    uploadDate = formats.get(formats.size() - 1)
+                            .getAsJsonObject().getAsJsonPrimitive("lastModified").getAsLong() / 1000;
+
+                try {
+                    actualUploadDate = new SimpleDateFormat("yyyy-mm-dd").parse(jsonObject.getAsJsonObject("microformat")
+                            .getAsJsonObject("playerMicroformatRenderer").getAsJsonPrimitive("uploadDate").getAsString());
+                } catch (ParseException ignore) {
+                }
+
+                JsonObject videoDetails = jsonObject.getAsJsonObject("videoDetails");
+                ownerId = videoDetails.getAsJsonPrimitive("channelId").getAsString();
+                ownerName = videoDetails.getAsJsonPrimitive("author").getAsString();
+                id = videoDetails.getAsJsonPrimitive("videoId").getAsString();
+                title = videoDetails.getAsJsonPrimitive("title").getAsString();
+
+                JsonArray thumbnailArray = videoDetails.getAsJsonObject("thumbnail").getAsJsonArray("thumbnails");
+                thumbnail = thumbnailArray.get(thumbnailArray.size() - 1).getAsJsonObject()
+                        .getAsJsonPrimitive("url").getAsString();
+
+                if (videoDetails.has("viewCount"))
+                    viewCountText = videoDetails.getAsJsonPrimitive("viewCount").getAsString();
+
+                lengthSeconds = videoDetails.getAsJsonPrimitive("lengthSeconds").getAsLong();
+                descriptionSnippet = videoDetails.getAsJsonPrimitive("shortDescription").getAsString();
+                live = videoDetails.getAsJsonPrimitive("isLiveContent").getAsBoolean();
             }
-        } else {
-            JsonArray formats = jsonObject.getAsJsonObject("streamingData").getAsJsonArray("formats");
-
-            if (formats != null)
-                uploadDate = formats.get(formats.size() -1)
-                    .getAsJsonObject().getAsJsonPrimitive("lastModified").getAsLong() / 1000;
-
-            try {
-                actualUploadDate = new SimpleDateFormat("yyyy-mm-dd").parse(jsonObject.getAsJsonObject("microformat")
-                        .getAsJsonObject("playerMicroformatRenderer").getAsJsonPrimitive("uploadDate").getAsString());
-            } catch (ParseException ignore) {}
-
-            JsonObject videoDetails = jsonObject.getAsJsonObject("videoDetails");
-            ownerId = videoDetails.getAsJsonPrimitive("channelId").getAsString();
-            ownerName = videoDetails.getAsJsonPrimitive("author").getAsString();
-            id = videoDetails.getAsJsonPrimitive("videoId").getAsString();
-            title = videoDetails.getAsJsonPrimitive("title").getAsString();
-
-            JsonArray thumbnailArray = videoDetails.getAsJsonObject("thumbnail").getAsJsonArray("thumbnails");
-            thumbnail = thumbnailArray.get(thumbnailArray.size() - 1).getAsJsonObject()
-                    .getAsJsonPrimitive("url").getAsString();
-
-            if (videoDetails.has("viewCount"))
-                viewCountText = videoDetails.getAsJsonPrimitive("viewCount").getAsString();
-
-            lengthSeconds = videoDetails.getAsJsonPrimitive("lengthSeconds").getAsLong();
-            descriptionSnippet = videoDetails.getAsJsonPrimitive("shortDescription").getAsString();
-            live = videoDetails.getAsJsonPrimitive("isLiveContent").getAsBoolean();
+        } catch (Exception exception) {
+            throw new NullPointerException(exception.getMessage() + "\nInternal Object: " + internalObject);
         }
     }
 
